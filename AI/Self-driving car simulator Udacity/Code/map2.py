3fafa2fc8e05fc4f3e3a38c57da6af6ba6ea9bb0
Phan Cong Hieu <86181500+TopdevVN@users.noreply.github.com>
2024-02-22 18:22:16 +0700

    Add files via upload

---
+++ AI/Self-driving car simulator Udacity/Code/map2.py
@@ -0,0 +1,55 @@
+print('Setting UP')
+
+# Import necessary libraries
+from data_preprocessing import *
+import socketio
+import eventlet
+import numpy as np
+from flask import Flask
+from tensorflow.keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
+import cv2
+import os
+
+# Disable debugging logs
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+sio = socketio.Server()
+
+app = Flask(__name__) # '__main__'
+maxSpeed = 15
+
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    speed = float(data['speed'])
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    image = np.asarray(image)
+    image = image_preprocessing(image)
+    image = np.array([image])
+    steering = float(model.predict(image))
+    throttle = 1.0 - speed / maxSpeed
+    print('{} {} {}'.format(steering, throttle, speed))
+    sendControl(steering, throttle)
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    print('Connected')
+    sendControl(0, 0)
+
+
+def sendControl(steering, throttle):
+    sio.emit('steer', data = {
+        'steering_angle': steering.__str__(),
+        'throttle': throttle.__str__()
+    })
+
+
+if __name__ == '__main__':
+    model = load_model('best.h5')
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
